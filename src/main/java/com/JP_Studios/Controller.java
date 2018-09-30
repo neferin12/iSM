c0f52fbc434968329e4fa8377b3bb21c549e1809
Neferin <neferin42@gmail.com>
2018-09-30 14:34:29 +0200

    Optimierung für hohe Iterationszahlen

--- src/main/java/com/JP_Studios/Controller.java
+++ src/main/java/com/JP_Studios/Controller.java
@@ -6,8 +6,8 @@ import com.JP_Studios.DeclarationClasses.VerteilerComparator;
 import java.util.ArrayList;
 import java.util.Arrays;
 
-/**
- * Created by Julian Pollinger
+/*
+  Created by Julian Pollinger
  */
 
 /**
@@ -18,7 +18,9 @@ public class Controller implements Runnable {
     private ArrayList<Kurs>[] kurses;
     private int iterations;
     private Oberflaeche oberflaeche;
-    private Verteiler verteiler[];
+
+
+    private static final int PART_SIZE = 200000;
 
     /**
      *
@@ -32,35 +34,50 @@ public class Controller implements Runnable {
         this.kurses = kurses;
         this.iterations = iterations;
         this.oberflaeche = oberflaeche;
-        verteiler = new Verteiler[iterations];
     }
 
     /**
      * Hier werden die einzelnen Durchläufe durchgeführt. Anschließend werden die Verteildurchläufe nach MimimiPunktzahl sortiert, und der mit den wenigsten Punkten wird nach Abschluss mit {@link Oberflaeche#verteilenFinished(Verteiler)} zurück an die {@link Oberflaeche} übermittelt.
-     * Während des Vorgangs wird auch der aktuelle Fortschritt mit {@link Oberflaeche#handleProgress(int)} an die {@link Oberflaeche} übermittelt.
+     * Während des Vorgangs wird auch der aktuelle Fortschritt mit {@link Oberflaeche#handleProgress(int)} an die {@link Oberflaeche} übermittelt. Hier wird die Anzahl nochmals in einzelne Blöcke zerlegt, die dann von der Funktion {@link Controller#calculatePart(int, int)} berechnet werden, damit der PC nicht überfordert ist.
      */
     @Override
     public void run() {
+        int rest = iterations % PART_SIZE;
+        int times = (iterations - rest) / PART_SIZE;
+
+        Verteiler[] results = new Verteiler[times + 1];
+
+        for (int i = 0; i < results.length - 1; i++) {
+            results[i] = calculatePart(i * PART_SIZE, (i + 1) * PART_SIZE);
+        }
+        results[results.length - 1] = calculatePart(iterations - (rest), iterations - 1);
+
+
+        Arrays.sort(results, new VerteilerComparator());
+        oberflaeche.verteilenFinished(results[0]);
+    }
+
+    /**
+     *
+     * @param lowerEnd An welcher Stelle in der GesamtIterationszahl der Algorythmus beginnen soll
+     * @param upperEnd An welcher Stelle in der GesamtIterationszahl der Algorythmus aufhören soll
+     * @return Gibt den besten Durchlauf aus diesem Abschnitt zurück
+     */
+
+    private Verteiler calculatePart(int lowerEnd, int upperEnd) {
+        Verteiler[] verteiler = new Verteiler[upperEnd - lowerEnd];
         for (int i = 0; i < verteiler.length; i++) {
             verteiler[i] = new Verteiler(schuelers, kurses);
         }
         for (int i = 0; i < verteiler.length; i++) {
             verteiler[i].seminareVerteilen();
-            double id= i;
-            double iterationsD =iterations;
-            double tpD = id/iterationsD;
-            int p = (int) (tpD* 100);
+            double iterationsD = iterations;
+            double tpD = (double) (i + lowerEnd) / iterationsD;
+            int p = (int) (tpD * 100);
             oberflaeche.handleProgress(p);
         }
-        Arrays.sort(verteiler,new VerteilerComparator());
-        oberflaeche.verteilenFinished(verteiler[0]);
+        Arrays.sort(verteiler, new VerteilerComparator());
+        return verteiler[0];
     }
 
-    /**
-     *
-     * @return Gibt eine Liste aller {@link Verteiler} zurück, die erstellt wurden.
-     */
-    public Verteiler[] getVerteiler() {
-        return verteiler;
-    }
 }
