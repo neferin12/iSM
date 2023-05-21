4a63d2b0977a0c8efd86134f7e023b6c33b3cc54
Julian Pollinger <julian@pollinger.dev>
2023-05-21 16:50:57 +0200

    benchmarking

---
+++ c_benchmark.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+cd c
+mkdir build || true
+cd build
+cmake .. -DCMAKE_BUILD_TYPE=Release
+make -j
+hyperfine \
+--warmup 3 \
+--parameter-list runs 100,1000,10000,100000,1000000 \
+--export-csv ../../c_benchmark.csv \
+'./cism ../example-files/Wahlen.csv ../example-files/Seminare.csv {runs}'
