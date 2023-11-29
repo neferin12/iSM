b71f659c193cc53e92767c8e7add81b71db14abe
Julian Pollinger <julian@pollinger.dev>
2023-11-29 22:25:20 +0100

    new measurements

--- benchmarking/c_multicore_benchmark.sh
+++ benchmarking/c_multicore_benchmark.sh
@@ -6,6 +6,6 @@ cmake .. -DCMAKE_BUILD_TYPE=Release
 make -j
 hyperfine \
 --warmup 3 \
---parameter-scan num_threads 1 8 \
+--parameter-scan num_threads 1 24 \
 --export-csv ../../benchmarking/results/c_multicore_benchmark.csv \
 'mpirun -np {num_threads} --use-hwthread-cpus ./cism ../../example-files/Wahlen.csv ../../example-files/Seminare.csv 1000000'
