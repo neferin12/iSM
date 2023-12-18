1670094bf10223b0e3607f9353404fd39d61f057
Julian Pollinger <julian@pollinger.dev>
2023-12-18 16:54:23 +0100

    somewhat working

--- node-native-addon/src/tsism_napi.cc
+++ node-native-addon/src/tsism_napi.cc
@@ -1,11 +1,24 @@
 #include <napi.h>
+extern "C" {
 #include <ism/algorithm.h>
+#include <ism/io.h>
+}
 
 using namespace Napi;
 
-Napi::String Method(const Napi::CallbackInfo& info) {
+Napi::Value Method(const Napi::CallbackInfo& info) {
   Napi::Env env = info.Env();
-  return Napi::String::New(env, str);
+  if (info.Length() < 2) {
+        Napi::TypeError::New(env, "Two arguments expected").ThrowAsJavaScriptException();
+        return env.Null();
+  }
+  if(!info[0].IsString() || !info[1].IsString()) {
+        Napi::TypeError::New(env, "Two string arguments expected").ThrowAsJavaScriptException();
+        return env.Null();
+  }
+
+  GArray *w_seminars = getSeminars(info[0].ToString().Utf8Value().c_str(), 'w');
+  return Napi::String::New(env, "lol");
 }
 
 Napi::Object Init(Napi::Env env, Napi::Object exports) {
--- node-native-addon/test/test_binding.js
+++ node-native-addon/test/test_binding.js
@@ -5,7 +5,7 @@ assert(TsismNapi, "The expected function is undefined");
 
 function testBasic()
 {
-    const result =  TsismNapi("hello");
+    const result =  TsismNapi("hello", "hello2");
     assert.strictEqual(result, "world", "Unexpected value returned");
 }
 
